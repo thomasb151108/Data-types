fa4097b6ce6efe79ebf81e1d0d3bb91312bdea43
thomasb151108 <thomasb151108@gmail.com>
2025-11-25 09:36:01 +0000

    first commit

---
+++ stack.py
@@ -0,0 +1,39 @@
+class Stack:
+    def __init__(self, _items):
+        self._items = []
+    
+    def is_empty(self):
+        if len(self._items) == 0:
+            empty = True
+        else:
+            empty = False
+        return empty
+    
+    def push(self, item):
+        self._items.append(item)
+
+    def pop(self):
+        if self.is_empty():
+            print("Stack is empty. Cannot pop item.")
+            return None
+        return self._items.pop()
+
+    def peek(self):
+        if self.is_empty():
+            print("Stack is empty. Cannot peek.")
+            return None
+        return self._items[-1]
+
+    def __len__(self):
+        return len(self._items)
+
+    def __str__(self):
+        return str(self._items)
+
+s = Stack(None)
+s.push(1)
+s.push(2)
+s.push(5)
+print(s)
+print(s.peek())
+print(len(s))
