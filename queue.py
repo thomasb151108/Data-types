1f4999ef79123f2a5f03efd8bedf82f7406d5056
thomasb151108 <thomasb151108@gmail.com>
2025-11-26 10:15:02 +0000

    queue

---
+++ queue.py
@@ -0,0 +1,42 @@
+class Queue:
+    def __init__(self): 
+        self.items = []
+    
+    def is_empty(self):
+        if len(self.items) == 0:
+            empty = True
+        else:
+            empty = False
+        return empty
+    
+    def enqueue(self, item):
+        self.items.append(item)
+    
+    def dequeue(self):
+        if self.is_empty():
+            print("Queue is empty")
+            return None
+        return self.items.pop(0)
+    
+    def peek(self):
+        if self.is_empty():
+            print("Queue is empty")
+            return None
+        return self.items[0]
+    
+    def __len__(self):
+        return len(self.items)
+
+    def __str__(self):
+        return str(self.items)
+
+q = Queue()
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(5)
+print(q)
+print(q.peek())
+print(len(q))
+q.dequeue()
+print(q)
+print(len(q))
